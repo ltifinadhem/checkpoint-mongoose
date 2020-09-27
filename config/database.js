3347fd24c46c7abe8833e9bf5f1593c1014a30d5
wassimbouchlema <67985356+wassimbouchlema@users.noreply.github.com>
2020-09-27 15:17:56 +0200

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,12 +1,9 @@
 const mongoose = require('mongoose')
 
-
-
-// })
 const Database=()=>{
     mongoose.connect('mongodb://localhost/persons',
     { useNewUrlParser: true, useUnifiedTopology: true}) 
-    .then(() => console.log('Connexion à MongoDB réussie !'))
-    .catch(() => console.log('Connexion à MongoDB échouée !'));}
+    .then(() => console.log('successfully connected  to the database  ! !'))
+    .catch(() => console.log('cannot connect to the database'));}
 
     module.exports= Database
