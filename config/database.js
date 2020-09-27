d817f8fe93250c30364a5b40786447617603997a
wassimbouchlema <wassimbouchlama@gmail.com>
2020-09-27 15:07:18 +0200

    first commit

---
+++ config/database.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+
+
+// })
+const Database=()=>{
+    mongoose.connect('mongodb://localhost/persons',
+    { useNewUrlParser: true, useUnifiedTopology: true}) 
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));}
+
+    module.exports= Database
